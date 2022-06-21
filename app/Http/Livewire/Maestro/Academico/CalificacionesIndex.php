da4fd6aee03c9ad6ba5d6516e07f4f573fcf22da
ZettForce <83797376+ZettForce@users.noreply.github.com>
2022-06-21 15:48:38 -0700

    1.3.1.1
    21/06 15:48

---
+++ app/Http/Livewire/Maestro/Academico/CalificacionesIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Maestro\Academico;
+
+use Livewire\Component;
+
+class CalificacionesIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.maestro.academico.calificaciones-index');
+    }
+}
